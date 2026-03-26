4df8b5ab2e67678268a469bc267b55f0ae0e064c
Vi <107654718+fifth-monger@users.noreply.github.com>
2026-03-26 11:38:11 -0500

    oops

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,15 +2,16 @@ import React from "react";
 import Synonyms from "./Synonyms.js";
 
 export default function Meaning(props) {
-  let definition = props.meaning.definitions.find(def => 
-  def.synonyms.length > 0
-) || props.meaning.definitions[0];
+  let definition =
+    props.meaning.definitions.find((def) => def.synonyms.length > 0) ||
+    props.meaning.definitions[0];
 
-return (
-  <div className="Meaning">
-    <p className="pos">{props.meaning.partOfSpeech}</p>
-    <p className="definition">{definition.definition}</p>
-    {definition.example && <p className="example">{definition.example}</p>}
-    <Synonyms synonyms={definition.synonyms} />
-  </div>
-);
+  return (
+    <div className="Meaning">
+      <p className="pos">{props.meaning.partOfSpeech}</p>
+      <p className="definition">{definition.definition}</p>
+      {definition.example && <p className="example">{definition.example}</p>}
+      <Synonyms synonyms={definition.synonyms} />
+    </div>
+  );
+}
