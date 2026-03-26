f5a262513a741db2060b25133741e16c4ca307d5
Vi <107654718+fifth-monger@users.noreply.github.com>
2026-03-26 17:02:49 -0500

    I believe the api key is working but i'm having a CORS issue

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -7,15 +7,28 @@ export default function Dictionary() {
   let [searchword, setSearchword] = useState("");
   let [results, setResults] = useState(null);
 
-  function handleResponse(response) {
-    console.log(response.data[0]);
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    console.log(response);
   }
 
   function search(event) {
     event.preventDefault();
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${searchword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey =
+      "tZuZvfO5JozSEC78MQ1uS6cW1a1EL6cTlz4DbeGAjaweJG8IjeWV7n6Z";
+
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${searchword}&per_page=3`;
+    axios
+      .get(pexelsApiUrl, {
+        headers: { Authorization: `Bearer ${pexelsApiKey}` },
+      })
+      .then(handlePexelsResponse);
   }
 
   function handleSearchwordChange(event) {
